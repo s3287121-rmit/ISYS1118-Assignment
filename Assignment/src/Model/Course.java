1f0d7a5e2cb3b6474ee4922c0be13a55359a8cd6
Jianwei Lai <s3599490@student.rmit.edu.au>
2018-04-06 19:07:59 +1000

    new changed

--- Assignment/src/Model/Course.java
+++ Assignment/src/Model/Course.java
@@ -1,5 +1,24 @@
 package Model;
 
 public class Course {
+	private String name;
+	private String courseCode;
 
+	public Course(String name, String courseCode) {
+		
+		this.name = name;
+		this.courseCode = courseCode;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCourseCode() {
+		return courseCode;
+	}
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
 }
--- Assignment/src/Model/Courselist.java
+++ Assignment/src/Model/Courselist.java
@@ -1,5 +1,21 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Courselist {
+private ArrayList<Course> courses;
+
+public Courselist(ArrayList<Course> courses) {
+	
+	this.courses = courses;
+}
+
+public ArrayList<Course> getCourses() {
+	return courses;
+}
+
+public void setCourses(ArrayList<Course> courses) {
+	this.courses = courses;
+}
 
 }
